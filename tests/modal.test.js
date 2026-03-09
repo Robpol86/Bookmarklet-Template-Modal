ceace6b485cf9269af10cffbb2e9015d1ce54239
Robpol86 <robpol86@gmail.com>
2026-03-09 15:24:11 +0900

    todo

--- tests/modal.test.js
+++ tests/modal.test.js
@@ -1,7 +1,23 @@
 import { describe, test } from "@jest/globals";
 
 describe("modal.mjs", () => {
+    describe("displayModal()", () => {
+        test.todo("closed by close button");
+
+        test.todo("closed by esc");
+
+        test.todo("closed by wrapped function");
+
+        test.todo("propagate abort via signal");
+
+        test.todo("bubble up exceptions");
+
+        test.todo("style removed from head");
+    });
+
     describe("modal()", () => {
         test.todo("closed by user");
+
+        test.todo("closed by wrapped function");
     });
 });
