d6087cdc4203b1a8ce67c096b6e447441c37f10b
Robpol86 <robpol86@gmail.com>
2026-03-10 17:18:27 +0900

    todo sleep tests

---
+++ tests/utils.test.js
@@ -0,0 +1,9 @@
+import { describe, test } from "@jest/globals";
+
+describe("utils.mjs", () => {
+    describe("sleep()", () => {
+        test.todo("sleep forever");
+
+        test.todo("sleep 1s");
+    });
+});
