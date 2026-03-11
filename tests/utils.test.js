589434760d3b6f4ad2acce7e18ea7843af942302
Robpol86 <robpol86@gmail.com>
2026-03-11 13:06:04 +0800

    Working on tests for sleep

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,9 +1,33 @@
-import { describe, test } from "@jest/globals";
+import { afterEach, beforeEach, describe, expect, jest, test } from "@jest/globals";
+import { sleep } from "../src/utils.mjs";
 
 describe("utils.mjs", () => {
     describe("sleep()", () => {
-        test.todo("sleep forever");
+        beforeEach(() => {
+            jest.useFakeTimers();
+        });
 
-        test.todo("sleep 1s");
+        afterEach(() => {
+            jest.clearAllTimers();
+            jest.useRealTimers();
+        });
+
+        test("sleep 60s", async () => {
+            let resolved = false;
+            const sleepPromise = sleep(60);
+            sleepPromise.then(() => (resolved = true));
+
+            jest.advanceTimersByTime(59900);
+            await Promise.resolve(); // Flush microtask queue.
+            expect(resolved).toBe(false);
+            // expect(sleepPromise).resolves.toBeDefined(); // TODO?
+
+            jest.advanceTimersByTime(100);
+            await Promise.resolve();
+            expect(resolved).toBe(true);
+            expect(sleepPromise).resolves.toBeUndefined();
+        });
+
+        test.todo("sleep forever");
     });
 });
